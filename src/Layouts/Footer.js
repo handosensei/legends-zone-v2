61e8135c4b518cc92fcf9dcbc321841f3e4567b2
handosensei <handosensei@gmail.com>
2023-04-30 09:41:46 +0200

    Footer

--- src/Layouts/Footer.js
+++ src/Layouts/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
                 <Container fluid>
                     <Row>
                         <Col sm={6}>
-                            {new Date().getFullYear()} © Velzon.
+                            {new Date().getFullYear()} © Meta-Legends.
                         </Col>
                         <Col sm={6}>
                             <div className="text-sm-end d-none d-sm-block">
-                                Design & Develop by Themesbrand
+                                Design & Develop by Meta-Legends
                             </div>
                         </Col>
                     </Row>
