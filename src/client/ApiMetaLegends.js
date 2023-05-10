ea10223afe129df4fc395bb5c4859947191941b8
handosensei <handosensei@gmail.com>
2023-05-10 21:47:48 +0200

    Refacto call api ML

--- src/client/ApiMetaLegends.js
+++ src/client/ApiMetaLegends.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 const apiUrl = process.env.REACT_APP_ML_API_URL;
 const apiKey = process.env.REACT_APP_ML_API_KEY;
 
-const getUrlTokenRewardsGet = (address) => {
-  return apiUrl + 'token-rewards/' + address + '/estimate';
+const getUrlRewardsEstimate = (address) => {
+  return `${apiUrl}rewards/${address}/estimate`;
 }
 
-export const getEstimateTokenRewards = async (address) => {
+export const getRewardsEstimate = async (address) => {
   try {
     const fake = '0x24DF9F5A2624Db695ee695399fd43DEB62c475Bd';
     return await axios({
       method: 'get',
       // url: getUrlTokenRewardsGet(address),
-      url: getUrlTokenRewardsGet(fake),
+      url: getUrlRewardsEstimate(fake),
       headers: { 'x-api-key': apiKey }
     });
   } catch (error) {
