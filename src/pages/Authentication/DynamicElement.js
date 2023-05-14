e995dc216bf73a15f22fc7e753026aa5a15b4b5f
handosensei <handosensei@gmail.com>
2023-05-14 08:38:14 +0200

    Logout

--- src/pages/Authentication/DynamicElement.js
+++ src/pages/Authentication/DynamicElement.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {DynamicContextProvider, DynamicWidget, FilterAndSortWallets} from "@dynamic-labs/sdk-react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {loginUser} from "../../store/auth/login/actions";
+import { logoutUser } from "../../store/actions";
+import PropTypes from "prop-types";
+import withRouter from "../../Components/Common/withRouter";
 
 const DynamicElement = ({props}) => {
   const dispatch = useDispatch();
 
   const [isHolder, setIsHolder] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
+
+  const { isUserLogout } = useSelector((state) => ({
+    isUserLogout: state.Login.isUserLogout,
+  }));
 
   const onConnectWallet = () => {
     if (!window.ethereum) {
@@ -36,10 +43,19 @@ const DynamicElement = ({props}) => {
     setIsHolder(true);
   }
 
+  useEffect(() => {
+
+  }, [dispatch]);
+
+
+
   return (
     <DynamicContextProvider
       settings={{
         environmentId: process.env.REACT_APP_DYNAMIC_AUTH,
+        appName: "Legends Zone",
+        siweStatement:"Welcome to LegendsZone. Signing is the only way we can truly know that you are the owner of the wallet you are connecting. Signing is a safe, gas-less transaction that does not in any way give this app permission to perform any transactions with your wallet.",
+        appLogoUrl: "https://meta-life.io/favicon.ico",
         // initialAuthenticationMode: 'connect-only',
         enableVisitTrackingOnConnectOnly: false,
         walletsFilter: FilterAndSortWallets(
@@ -58,6 +74,7 @@ const DynamicElement = ({props}) => {
             onConnectWallet();
           },
           onLogout: (args) => {
+            dispatch(logoutUser());
             window.location = process.env.PUBLIC_URL + "/";
           }
         }
