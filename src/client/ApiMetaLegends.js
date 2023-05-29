0f5f77774cd0cae03fe63b3513d132cb58332147
handosensei <handosensei@gmail.com>
2023-05-29 23:39:59 +0200

    Refacto call ML API

--- src/client/ApiMetaLegends.js
+++ src/client/ApiMetaLegends.js
@@ -2,53 +2,35 @@ import axios from 'axios';
 const apiUrl = process.env.REACT_APP_ML_API_URL;
 const apiKey = process.env.REACT_APP_ML_API_KEY;
 
-const getUrlRewardsEstimate = () => {
-  return `${apiUrl}rewards`;
-}
-
-const getUrlLegends = () => {
-  return `${apiUrl}legends`;
-}
-
-const getUrlEligibilityOgPet = () => {
-  return `${apiUrl}eligibility/og-pets`;
+const get = async (url) => {
+  const user = JSON.parse(sessionStorage.getItem("authUser"));
+  try {
+    return await axios({
+      method: 'get',
+      url: url,
+      headers: { 'x-api-key': apiKey, 'Authorization': `Bearer ${user.jwt}` }
+    });
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export const getRewardsEstimate = async () => {
-  const user = JSON.parse(sessionStorage.getItem("authUser"));
-  try {
-    return await axios({
-      method: 'get',
-      url: getUrlRewardsEstimate(),
-      headers: { 'x-api-key': apiKey, 'Authorization': `Bearer ${user.jwt}` }
-    });
-  } catch (error) {
-    console.log(error)
-  }
+  const url = `${apiUrl}rewards`;
+  return get(url);
 }
 
 export const getLegends = async () => {
-  const user = JSON.parse(sessionStorage.getItem("authUser"));
-  try {
-    return await axios({
-      method: 'get',
-      url: getUrlLegends(),
-      headers: { 'x-api-key': apiKey, 'Authorization': `Bearer ${user.jwt}` }
-    });
-  } catch (error) {
-    console.log(error)
-  }
+  const url = `${apiUrl}legends`;
+  return get(url);
 }
 
 export const getEligibilityOgPets = async () => {
-  const user = JSON.parse(sessionStorage.getItem("authUser"));
-  try {
-    return await axios({
-      method: 'get',
-      url: getUrlEligibilityOgPet(),
-      headers: { 'x-api-key': apiKey, 'Authorization': `Bearer ${user.jwt}` }
-    });
-  } catch (error) {
-    console.log(error)
-  }
+  const url = `${apiUrl}eligibility/og-pets`;
+  return get(url);
+}
+
+export const isHolder = async () => {
+  const url = `${apiUrl}users/is-holder`;
+  return get(url);
 }
