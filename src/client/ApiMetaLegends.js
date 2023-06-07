d7a046074a826495584fd001e16623676ff88fea
handosensei <handosensei@gmail.com>
2023-06-07 23:11:55 +0200

    PATCH mint orders

--- src/client/ApiMetaLegends.js
+++ src/client/ApiMetaLegends.js
@@ -2,19 +2,35 @@ import axios from 'axios';
 const apiUrl = process.env.REACT_APP_ML_API_URL;
 const apiKey = process.env.REACT_APP_ML_API_KEY;
 
-const get = async (url) => {
+const buildHeader = () => {
   const user = JSON.parse(sessionStorage.getItem("authUser"));
-
   const headers = { 'x-api-key': apiKey }
   if (user != null) {
     headers['Authorization'] = `Bearer ${user.jwt}`;
   }
 
+  return headers;
+}
+
+const get = async (url) => {
   try {
     return await axios({
       method: 'get',
       url: url,
-      headers: headers,
+      headers: buildHeader(),
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const patch = async (url, payload) => {
+  try {
+    return await axios({
+      method: 'patch',
+      url: url,
+      headers: buildHeader(),
+      data: payload
     });
   } catch (error) {
     console.log(error)
@@ -44,4 +60,9 @@ export const isHolder = async (address) => {
 export const getMintOrderOgPets = async () => {
   const url = `${apiUrl}mint-orders/og-pets`;
   return get(url);
+}
+
+export const patchMintOrdersOgPets = async (payload) => {
+  const url = `${apiUrl}mint-orders/og-pets`;
+  return patch(url, payload);
 }
