c49bf16fef05e91a705f468b71b66238598c0b24
handosensei <handosensei@gmail.com>
2023-09-11 23:11:07 +0200

    Open mint

--- src/pages/Claim/HoldingRewards/Reward.js
+++ src/pages/Claim/HoldingRewards/Reward.js
@@ -105,8 +105,7 @@ const Reward = ({asset, contract, account}) => {
 
   const ClaimButton = () => {
     if (remainingToClaim > 0) {
-      // return (<button className="btn btn-primary" onClick={() => { claim(); }}>Claim</button>);
-      return (<button className="btn btn-primary">Claim</button>);
+      return (<button className="btn btn-primary" onClick={() => { claim(); }}>Claim</button>);
     }
 
     return (<button className="btn btn-light">Claim</button>);
