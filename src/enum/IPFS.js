6272f147928cc9054c76a3ce95fbcab81681dd38
handosensei <handosensei@gmail.com>
2023-10-01 22:10:31 +0200

    update CID LZ reward

--- src/enum/IPFS.js
+++ src/enum/IPFS.js
@@ -3,7 +3,7 @@ export const PINATA_ML = 'https://metalegends.mypinata.cloud/ipfs'
 // MetaLegends
 
 // Legends zone reward
-export const CID_LZREWARD_CONTENT = 'QmWpskk3QApuTEHG7fUKhV1AcvrCwjkLf6xSxuJnavp7AV';
+export const CID_LZREWARD_CONTENT = process.env.REACT_APP_LZREWARD_CONTENT_CID;
 export const LZREWARD_PATH_GIF = `${PINATA_ML}/${CID_LZREWARD_CONTENT}/gif/`;
 
 // OG Pets
